9a28d105b4a547ee7e9f61f5f1a09e40babf34b3
Gunjet <gunjegunje54@gmail.com>
2025-08-25 17:34:38 +0800

    fix

--- components/pages/improvement-result/item1-table.tsx
+++ components/pages/improvement-result/item1-table.tsx
@@ -20,82 +20,70 @@ export default function Item1Table() {
               <TableCell
                 className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
                 colSpan={1}
-                rowSpan={4}
-              >
+                rowSpan={4}>
                 コピー
               </TableCell>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 メインコピー
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 AI技術でビジネスを革新し、未来の成長を共に築く
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 サブコピー1
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 デジタルトランスフォーメーションを加速するためのパートナーシップ
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 サブコピー2
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 AIを通じて企業の潜在能力を解き放つ
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 サブコピー3
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 次世代のビジネスチャンスを掴むためのソリューションを提供
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold">
                 ビジュアル
               </TableCell>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold">
                 作成指示
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 <div className="flex flex-row items-center justify-between space-x-2 text-[13px]]">
                   <p>
                     AI技術のテーマを反映した未来的な都市の写真に、デジタルデータやフローを上から重ねてください。
@@ -106,8 +94,9 @@ export default function Item1Table() {
                         <GlobeIcon className="size-3" stroke="#777" />
                       </Button>
                     </TooltipTrigger>
-                    <TooltipContent>
-                      情報ソース： https://wwwwwwwwwwwwwwwwwwwww
+                    <TooltipContent className="max-w-[241px]">
+                      情報ソース： https://wwwwwwwwwwwwwwwwww,
+                      https://wwwwwwwwwwwwwwwwww, https://wwwwwwwwwwwwww...
                     </TooltipContent>
                   </Tooltip>
                 </div>
@@ -117,34 +106,29 @@ export default function Item1Table() {
               <TableCell
                 colSpan={1}
                 rowSpan={2}
-                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold">
                 CTAボタン
               </TableCell>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 ボタンテキスト
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 詳しく知る
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] font-semibold">
                 マイクロコピー
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 <div className="flex flex-row items-center justify-between space-x-2 text-[13px]]">
                   <p>AI技術で次のステップを踏み出す</p>
                   <Tooltip>
@@ -153,8 +137,8 @@ export default function Item1Table() {
                         <LightbulbIcon className="size-3" stroke="#777" />
                       </Button>
                     </TooltipTrigger>
-                    <TooltipContent>
-                      AIのアイデア： AIのアイデアAIのアイデアAIのアイデアAIの
+                    <TooltipContent className="max-w-[241px]">
+                      自社・他社いずれのLPにも情報源が含まれていない、AIのアイデアです。
                     </TooltipContent>
                   </Tooltip>
                 </div>
@@ -163,20 +147,17 @@ export default function Item1Table() {
             <TableRow>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold">
                 権威付け
               </TableCell>
               <TableCell
                 colSpan={1}
-                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold"
-              >
+                className="border border-[#ccc] bg-[#fafafa] align-middle font-semibold">
                 作成指示
               </TableCell>
               <TableCell
                 colSpan={3}
-                className="border border-[#ccc] bg-white p-2"
-              >
+                className="border border-[#ccc] bg-white p-2">
                 <div className="flex flex-row items-center space-x-2 text-[13px]">
                   <p>
                     AI技術のテーマを反映した未来的な都市の写真に、デジタルデータやフローを上から重ねてください。
